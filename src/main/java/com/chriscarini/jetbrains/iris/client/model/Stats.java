e5b218cf0f2dbd50a06e36ed52e9d60fd41d07c6
Chris Carini <6374067+ChrisCarini@users.noreply.github.com>
2022-06-03 08:36:56 -0700

    Handle list of 7 week responses from stats API. (#49)
    * Handle list of 7 week responses from stats API.
    
    Fixes #24

--- src/main/java/com/chriscarini/jetbrains/iris/client/model/Stats.java
+++ src/main/java/com/chriscarini/jetbrains/iris/client/model/Stats.java
@@ -2,10 +2,14 @@ package com.chriscarini.jetbrains.iris.client.model;
 
 import com.chriscarini.jetbrains.iris.plugin.messages.IrisMessages;
 import com.google.gson.annotations.SerializedName;
+import org.jetbrains.annotations.NonNls;
+
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
+import java.util.List;
 import java.util.Locale;
-import org.jetbrains.annotations.NonNls;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 
 /**
@@ -22,54 +26,84 @@ public class Stats {
   private static final String TABLE_BODY_HTML = "</table></body></html>";
   // @formatter:off
   private static final String HTML_FORMAT = HTML_BODY_TABLE +
-      String.format(ROW_HEADER, IrisMessages.get("iris.stats.table.median.seconds.to.claim.last.month"), ROW_VAL) +
-      String.format(ROW_HEADER, IrisMessages.get("iris.stats.table.incidents.claimed.last.month"), ROW_VAL) +
+      String.format(ROW_HEADER, IrisMessages.get("iris.stats.table.median.seconds.to.claim.last.week"), ROW_VAL) +
+      String.format(ROW_HEADER, IrisMessages.get("iris.stats.table.incidents.claimed.last.week"), ROW_VAL) +
       String.format(ROW_HEADER, IrisMessages.get("iris.stats.table.total.active.users"), ROW_VAL) +
       String.format(ROW_HEADER, IrisMessages.get("iris.stats.table.total.applications"), ROW_VAL) +
+      String.format(ROW_HEADER, IrisMessages.get("iris.stats.table.total.high.priority.incidents.last.week"), ROW_VAL) +
       String.format(ROW_HEADER, IrisMessages.get("iris.stats.table.total.incidents"), ROW_VAL) +
-      String.format(ROW_HEADER, IrisMessages.get("iris.stats.table.total.incidents.today"), ROW_VAL) +
+      String.format(ROW_HEADER, IrisMessages.get("iris.stats.table.total.incidents.last.week"), ROW_VAL) +
       String.format(ROW_HEADER, IrisMessages.get("iris.stats.table.total.messages.sent"), ROW_VAL) +
-      String.format(ROW_HEADER, IrisMessages.get("iris.stats.table.total.messages.sent.today"), ROW_VAL) +
+      String.format(ROW_HEADER, IrisMessages.get("iris.stats.table.total.messages.sent.last.week"), ROW_VAL) +
       String.format(ROW_HEADER, IrisMessages.get("iris.stats.table.total.plans"), ROW_VAL)
       + TABLE_BODY_HTML;
   // @formatter:on
 
-  @SerializedName("median_seconds_to_claim_last_month") //NON-NLS
-  private double medianSecondsToClaimLastMonth;
-  @SerializedName("pct_incidents_claimed_last_month") //NON-NLS
-  private double pctIncidentsClaimedLastMonth;
+  // Define the maximum number of decimals (number of symbols #); shows symbol '.' instead of ','
+  private static final DecimalFormat DF = new DecimalFormat("#.##", new DecimalFormatSymbols(Locale.US));
+
+  @SerializedName("median_seconds_to_claim_last_week") //NON-NLS
+  List<Map<String, Double>> medianSecondsToClaimLastWeek;
+  @SerializedName("pct_incidents_claimed_last_week") //NON-NLS
+  List<Map<String, Double>> pctIncidentsClaimedLastWeek;
   @SerializedName("total_active_users") //NON-NLS
-  private double totalActiveUsers;
+  List<Map<String, Double>> totalActiveUsers;
   @SerializedName("total_applications") //NON-NLS
-  private double totalApplications;
+  List<Map<String, Double>> totalApplications;
+  @SerializedName("total_high_priority_incidents_last_week") //NON-NLS
+  List<Map<String, Double>> totalHighPriorityIncidentsLastWeek;
   @SerializedName("total_incidents") //NON-NLS
-  private double totalIncidents;
-  @SerializedName("total_incidents_today") //NON-NLS
-  private double totalIncidentsToday;
+  List<Map<String, Double>> totalIncidents;
+  @SerializedName("total_incidents_last_week") //NON-NLS
+  List<Map<String, Double>> totalIncidentsLastWeek;
   @SerializedName("total_messages_sent") //NON-NLS
-  private double totalMessagesSent;
-  @SerializedName("total_messages_sent_today") //NON-NLS
-  private double totalMessagesSentToday;
+  List<Map<String, Double>> totalMessagesSent;
+  @SerializedName("total_messages_sent_last_week") //NON-NLS
+  List<Map<String, Double>> totalMessagesSentLastWeek;
   @SerializedName("total_plans") //NON-NLS
-  private double totalPlans;
+  List<Map<String, Double>> totalPlans;
 
   @Override
   public String toString() {
-    //noinspection HardCodedStringLiteral
-    return "Stats{" + "medianSecondsToClaimLastMonth=" + medianSecondsToClaimLastMonth
-        + ", pctIncidentsClaimedLastMonth=" + pctIncidentsClaimedLastMonth + ", totalActiveUsers=" + totalActiveUsers
-        + ", totalApplications=" + totalApplications + ", totalIncidents=" + totalIncidents + ", totalIncidentsToday="
-        + totalIncidentsToday + ", totalMessagesSent=" + totalMessagesSent + ", totalMessagesSentToday="
-        + totalMessagesSentToday + ", totalPlans=" + totalPlans + '}';
+    return "Stats{" +
+        "medianSecondsToClaimLastWeek=" + medianSecondsToClaimLastWeek +
+        ", pctIncidentsClaimedLastWeek=" + pctIncidentsClaimedLastWeek +
+        ", totalActiveUsers=" + totalActiveUsers +
+        ", totalApplications=" + totalApplications +
+        ", totalIncidents=" + totalIncidents +
+        ", totalHighPriorityIncidentsLastWeek=" + totalHighPriorityIncidentsLastWeek +
+        ", totalIncidentsLastWeek=" + totalIncidentsLastWeek +
+        ", totalMessagesSent=" + totalMessagesSent +
+        ", totalMessagesSentLastWeek=" + totalMessagesSentLastWeek +
+        ", totalPlans=" + totalPlans +
+        '}';
   }
 
   public String toHtml() {
-    // Define the maximum number of decimals (number of symbols #); shows symbol '.' instead of ','
-    final DecimalFormat df = new DecimalFormat("#.##", new DecimalFormatSymbols(Locale.US));
+    return String.format(HTML_FORMAT,
+        valuesToRowCells(medianSecondsToClaimLastWeek),
+        valuesToRowCells(pctIncidentsClaimedLastWeek),
+        valuesToRowCells(totalActiveUsers),
+        valuesToRowCells(totalApplications),
+        valuesToRowCells(totalHighPriorityIncidentsLastWeek),
+        valuesToRowCells(totalIncidents),
+        valuesToRowCells(totalIncidentsLastWeek),
+        valuesToRowCells(totalMessagesSent),
+        valuesToRowCells(totalMessagesSentLastWeek),
+        valuesToRowCells(totalPlans)
+    );
+  }
 
-    return String.format(HTML_FORMAT, df.format(medianSecondsToClaimLastMonth), df.format(pctIncidentsClaimedLastMonth),
-        df.format(totalActiveUsers), df.format(totalApplications), df.format(totalIncidents),
-        df.format(totalIncidentsToday), df.format(totalMessagesSent), df.format(totalMessagesSentToday),
-        df.format(totalPlans));
+  private String valuesToRowCells(final List<Map<String, Double>> listOfMaps) {
+    if (listOfMaps == null) {
+      return "N/A";
+    }
+    return listOfMaps.stream().map(a -> {
+      // There should only be a single value in the result, and if it is null, treat it as a zero.
+      if (a.values().toArray()[0] == null) {
+        return 0.0;
+      }
+      return a.values().stream().findFirst().orElse(0.0);
+    }).map(DF::format).collect(Collectors.joining("</td><td>"));
   }
 }
